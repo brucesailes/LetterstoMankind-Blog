7f4c862ade87b4bf572ddba386ceb64191cfcfa7
investgod <110871245+investgod@users.noreply.github.com>
2024-04-03 18:31:00 -0500

    You know what it is

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import HeroPost from "../components/hero-post";
 import MoreStories from "../components/more-stories";
 import { GetStaticProps } from "next";
 import { getAllPostsForHome } from "../lib/api";
+import { useRouter } from 'next/router';
 
 interface Post {
   title: string;
@@ -26,6 +27,13 @@ export default function Home({ allPosts: { edges }, preview }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className="container mx-auto px-4">
+      <div className="text-center mb-8">
+          <Link href="https://brucesailes.com" passHref legacyBehavior>
+            <a target="_blank" rel="noopener noreferrer" className="inline-block bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300 ease-in-out transform hover:-translate-y-1">
+              Home
+            </a>
+          </Link>
+        </div>
         <h1 className="text-4xl font-bold text-center mb-8 text-blue-600 transition-transform transform hover:scale-105">The Letters to Mankind</h1>
         <div className="border border-gray-300 rounded-lg shadow-lg bg-white p-6 mb-8 transition-transform transform hover:scale-105">
           <p className="text-2xl font-bold text-center mb-8 text-blue-600 transition-transform transform hover:scale-105">BEFORE YOU READ</p>
